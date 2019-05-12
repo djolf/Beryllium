8f6291c5aca76d481401ed8b3cf9c68c8374741a
lin lifan <lifan1312@gmail.com>
2019-05-12 12:17:49 +0800

    optimized video updated packages removed unused components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import Hero from './Hero';
 import './App.css';
+import bgmp4 from './assets/background.mp4';
+import poster from './assets/background.jpg';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  
+  render() {
+    return(
+      <div className='root'>
+        <header className='v-header container'>
+          <div className="fullscreen-video-wrap">
+            <video src={bgmp4} autoPlay={true} loop={true} poster={poster}></video>
+          </div>
+          <div className="header-overlay"></div>
+          <div className="header-content">
+            <h1>Djolf's Page</h1>
+            <p>This page is a coding practice by djolf.</p>
+          </div>
+          <div className="frontpage-btn">More..</div>
+        </header>
+      </div>
+    );
+  }
 
-    render() {
-        return(
-            <div className='root'>
-                <Hero/>
-            </div>
-        );
-    }
 }
 
 export default App;
